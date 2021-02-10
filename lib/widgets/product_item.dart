2b6caf702b8195f27187ec1db5ec603f592f1881
Aashu_Jha <aashish2000kumar@gmail.com>
2021-02-11 00:34:37 +0530

    fix: added animation in product-itemImage

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -19,7 +19,11 @@ class ProductItem extends StatelessWidget {
             onTap: () {
               Navigator.pushNamed(context, ProductsDetailScreen.routeName, arguments: product.id);
             },
-              child: Image.network(product.imageUrl, fit: BoxFit.cover,)
+              child: FadeInImage(
+                  placeholder: AssetImage('assets/images/product-placeholder.png'),
+                  image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              )
           ),
         footer: GridTileBar(
           backgroundColor: Colors.black54,
