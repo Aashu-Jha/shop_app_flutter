7b6e5b339fc311a1ffe4d9a6141b8f497016156d
Aashu_Jha <aashish2000kumar@gmail.com>
2021-02-11 01:17:35 +0530

    feat: Converted to CustomScrollView

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -10,33 +10,43 @@ class ProductsDetailScreen extends StatelessWidget {
     final productID = ModalRoute.of(context).settings.arguments as String;
     final loadedProduct = Provider.of<Products>(context, listen: false).findById(productID);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              Hero(
-                tag: loadedProduct.id,
-                child: Image(image: NetworkImage(loadedProduct.imageUrl),
-                  height: 300,
-                  width: double.infinity,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text(loadedProduct.title),
+              expandedHeight: 300,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(loadedProduct.title),
+                background: Hero(
+                  tag: loadedProduct.id,
+                  child: Image(image: NetworkImage(loadedProduct.imageUrl),
+                    height: 300,
+                    width: double.infinity,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10,),
-              Text('\$ ${loadedProduct.price}'),
-              SizedBox(height: 10,),
-              Text('${loadedProduct.description}',
-               textAlign: TextAlign.center,
-               softWrap: true,
-               style: TextStyle(
-                 fontSize: 20,
-                 fontWeight: FontWeight.w500,
-               ),)
-            ],
-          ),
+              )
+            ),
+            SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    SizedBox(height: 10,),
+                    Center(child: Text('\$ ${loadedProduct.price}')),
+                    SizedBox(height: 10,),
+                    Text('${loadedProduct.description}',
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 800,)
+                  ]
+                ),
+            )
+          ],
         ),
       ),
     );
